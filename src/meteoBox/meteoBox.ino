e80f446bab81100b44236dfd4c8f151aecb418ea
n.segen <n.segen@invento.by>
2023-02-26 22:40:04 +0300

    Added BME280 sensor integration

--- src/meteoBox/meteoBox.ino
+++ src/meteoBox/meteoBox.ino
@@ -1,12 +1,8 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
-
-#ifdef U8X8_HAVE_HW_SPI
-#include <SPI.h>
-#endif
-#ifdef U8X8_HAVE_HW_I2C
 #include <Wire.h>
-#endif
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 U8G2_ST7565_ERC12864_ALT_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 8, /* data=*/ 9, /* cs=*/ 5, /* dc=*/ 7, /* reset=*/ 6);
 
@@ -49,7 +45,7 @@ class ClockWidget : public BaseWidget<char*> {
     }
 };
 
-class TemperatureWidget : public BaseWidget<char*> {
+class TemperatureWidget : public BaseWidget<uint8_t> {
   public:
     using BaseWidget::BaseWidget;
   protected:
@@ -58,11 +54,12 @@ class TemperatureWidget : public BaseWidget<char*> {
     }
 };
 
-class HumidityWidget : public BaseWidget<char*> {
+class HumidityWidget : public BaseWidget<uint8_t> {
   public:
     using BaseWidget::BaseWidget;
   protected:
     void drawBody() override {
+      char
       u8g2.drawStr(x, y, value);
     }
 };
@@ -85,17 +82,16 @@ class CarbonDioxideLevelWidget : public BaseWidget<char> {
     }
 };
 
-void setup(void) {
-  u8g2.begin();
-  u8g2.setContrast(80);
-  u8g2.enableUTF8Print();	
-}
+#define SEALEVELPRESSURE_HPA (1013.25)
 
+Adafruit_BME280 bme = Adafruit_BME280(); // I2C
 char weatherIcon = 0;
 char* time = "23:09";
-char* degree = "22°C";
-char* humidity = "100%";
+uint8_t degree; //"22°C";
+uint8_t humidity; //"100%";
 char currentEmotic = 0;
+bool status;
+
 
 ClockWidget clockWidget = ClockWidget(38, 32, u8g2_font_logisoso32_tf);
 WeatherWidget weatherWidget = WeatherWidget(0, 32, u8g2_font_open_iconic_weather_4x_t);
@@ -104,8 +100,38 @@ HumidityWidget humidityWidget = HumidityWidget(74, 63, u8g2_font_logisoso20_tf);
 TemperatureWidget temperatureWidget = TemperatureWidget(0, 63, u8g2_font_logisoso20_tf);
 Widget* allWidgets[] = { &clockWidget, &weatherWidget, &carbonDioxideLevelWidget, &humidityWidget, &temperatureWidget };
 
+void setup(void) {
+  Serial.begin(9600);
+  
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin(BME280_ADDRESS_ALTERNATE);  
+  u8g2.begin();
+  u8g2.setContrast(80);
+  u8g2.enableUTF8Print();
+}
+
+void readSensors() {
+  bme.takeForcedMeasurement();
+  degree = bme.readTemperature();
+  humidity = bme.readHumidity();
+  // dispTemp = bme.readTemperature();
+  // dispHum = bme.readHumidity();
+  // dispPres = (float)bme.readPressure() * 0.00750062;
+  // dispCO2 = mhz19.getPPM();
+
+  // if (dispCO2 < 800) setLED(2);
+  // else if (dispCO2 < 1200) setLED(3);
+  // else if (dispCO2 >= 1200) setLED(1);
+}
+
 void loop(void) {
   u8g2.clearBuffer();
+
+  if (!status) {
+    Serial.println(F("Could not find a valid BME280 sensor, check wiring!"));
+  }
+  readSensors();
 
   weatherWidget.setValue(weatherIcon);
   clockWidget.setValue(time);
